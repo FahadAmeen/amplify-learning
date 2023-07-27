d2ac6871e4e488bebacc0e8e7751379fcb7ce351
fahadameen <m.fahadameen@gmail.com>
2023-07-27 23:01:57 +0500

    set up full stack app

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { Amplify } from 'aws-amplify'
+import awsExports from './aws-exports'
+
 import App from './App.vue'
 import router from './router'
 
+Amplify.configure(awsExports)
 const app = createApp(App)
 
 app.use(createPinia())
